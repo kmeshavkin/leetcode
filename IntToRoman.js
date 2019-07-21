b1e872d6183f79e1a087d2ae52cf60627de69543
Kostyan1996 <meshavkin1996@gmail.com>
2019-07-21 22:20:32 +0300

    Initial commit

---
+++ IntToRoman.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function (num) {
+  const rule = ['', 'o', 'oo', 'ooo', 'of', 'f', 'fo', 'foo', 'fooo', 'ot']; // o is ones, f is fives, t is tens
+};
+
+console.log(intToRoman('3', 'III'));
